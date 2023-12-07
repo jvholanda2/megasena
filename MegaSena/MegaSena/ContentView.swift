679816d4bb61e3d800274652a397a84cd7a2efc7
jvholanda2 <jvholanda18@gmail.com>
2023-12-07 19:15:37 -0300

    Inicia ao projeto

---
+++ MegaSena/MegaSena/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MegaSena
+//
+//  Created by João Vitor Alves Holanda on 07/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MegaSena/MegaSena/MegaSenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegaSenaApp.swift
+//  MegaSena
+//
+//  Created by João Vitor Alves Holanda on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MegaSenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
